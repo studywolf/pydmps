16592a90547d1b787ae2d41576db2c19a2ae592c
Travis DeWolf <travis.dewolf@gmail.com>
2014-08-07 12:40:50 -0400

    Adding files for PyPi upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'pydmps',
+  packages = ['pydmps'], 
+  version = '0.1',
+  description = 'Python implementation of Dynamic Movement Primitives (DMPs)',
+  author = 'Travis DeWolf',
+  author_email = 'travis.dewolf@gmail.com',
+  url = 'https://github.com/studywolf/pydmps',
+  download_url = 'https://github.com/studywolf/pydmps/tarball/0.1'
+  keywords = ['dmps', 'trajectory generation', 'control'],
+  classifiers = [],
+)
