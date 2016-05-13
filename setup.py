84aeb312c790785e75f6bdce972c7fc5ab4dabf7
travis dewolf <travis.dewolf@gmail.com>
2016-05-13 15:00:03 -0400

    updated setup.py to setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,26 @@
-from distutils.core import setup
+#!/usr/bin/env python
+import imp
+import io
+import sys
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+
+from setuptools import find_packages, setup  
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
 
 setup(
   name = 'pydmps',
@@ -9,6 +31,6 @@ setup(
   author_email = 'travis.dewolf@gmail.com',
   url = 'https://github.com/studywolf/pydmps',
   download_url = 'https://github.com/studywolf/pydmps/tarball/0.1',
-  keywords = ['dmps', 'trajectory generation', 'control'],
+  keywords = ['dmps', 'dynamic movement primitives', 'trajectory generation', 'control'],
   classifiers = [],
 )
